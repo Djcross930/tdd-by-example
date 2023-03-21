4180a8843af698dbe48cdafd5398696336a88f58
djcross930 <dancross96@gmail.com>
2023-03-21 10:56:43 -0500

    Refactored both sub classes to be more present in the moeny class

--- src/main/java/guru/springframework/Franc.java
+++ src/main/java/guru/springframework/Franc.java
@@ -1,17 +1,9 @@
 package guru.springframework;
 
 public class Franc extends Money {
-    private String currency;
 
     public Franc(int amount, String currency) {
-        this.amount = amount;
-        this.currency = currency;
+        super(amount, currency);
     }
-    @Override
-    protected String currency() {
-        return currency;
-    }
-    public Money times(int multiplier) {
-        return Money.franc(amount * multiplier);
-    }
+
 }
--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -1,19 +1,43 @@
 package guru.springframework;
 
-public abstract class Money {
+public class Money {
     protected int amount;
-    protected abstract String currency();
-    public abstract Money times(int multiplier);
-    public static Franc franc(int amount) {
-        return new Franc(amount, "CHF");
-    }
-    public static Dollar dollar(int amount) {
-        return new Dollar(amount, "USD");
+    protected String currency;
 
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    protected String currency() {
+        return currency;
+    }
+
+
+    public static Money dollar(int amount){
+        return new Dollar(amount, "USD");
+    }
+
+    public static Money franc(int amount){
+        return new Franc(amount, "CHF");
     }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount && this.getClass().equals(object.getClass());
+        return amount == money.amount
+                && this.currency == money.currency;
     }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+
+    public Money times(int multiplier) {
+        return new Money(amount * multiplier, this.currency);
+    }
+
 }
